f877eb40bc80c9a25186200295b07c771f824a7d
Lika Lex <lika.lex555@gmail.com>
2018-05-28 17:37:31 +0300

    Add Subject class

--- Gemfile
+++ Gemfile
@@ -5,5 +5,5 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "telegram-bot-ruby"
-gem "redis"
+gem 'redis', '~> 3.2'
 gem 'dotenv-rails', groups: [:development, :test]
--- main.rb
+++ main.rb
@@ -17,14 +17,14 @@ class BaseClass
     @name = message.from.first_name
   end
 
-  def sendMessage(text)
-    bot.api.sendMessage(chat_id: @message.chat.id, text: text)
+  def send_message(text)
+    bot.api.send_message(chat_id: @message.chat.id, text: text)
   end
 
 
   def valid_date?(date_string)
     date = Date.parse(date_string).to_s
-    year, month, day = date.split "-"
+    year, month, day = date.split '-'
     Date.valid_date? year.to_i, month.to_i, day.to_i
     return true
   rescue
@@ -43,41 +43,44 @@ class BaseClass
     end
   end
 
+  def calculator(tasks)
+    days_per_task = @sum_of_days / tasks
+    days_gone = @sum_of_days - @remainder
+    @accomplished = days_gone / days_per_task
+  end
+
 end
 
 class Start < BaseClass
   def run
-    sendMessage("Привет. Я тебе смогу помочь сдать все лабы, чтобы мамка не ругалась. Смотри список что я умею:
+    send_message('Привет. Я тебе смогу помочь сдать все лабы, чтобы мамка не ругалась. Смотри список что я умею:
 /semester - запоминает даты начала и конца семестра
 /subject - добавляет предмет и количество лабораторных работ по нему
 /status - выводит твой список лаб, которые тебе предстоит сдать
-/reset - сбрасывает для пользователя все данные.")
+/reset - сбрасывает для пользователя все данные.')
   end
 end
 
-
-
-
 class Semester < BaseClass
-  def first_step
-    sendMessage("Когда начинаем учиться?(Формат: ГГГГ-ММ-ДД)")
+  def run
+    send_message('Когда начинаем учиться?(Формат: ГГГГ-ММ-ДД)')
     @bot.listen do |message|
-      if valid_date?(message.text) == false then sendMessage(
+      if valid_date?(message.text) == false then send_message(
         "#{@name}, ты неверно ввел дату!")
       else
         @start_date = Date.parse(message.text)
 
-        sendMessage("Когда заканчиваем учиться?(Формат: ГГГГ-ММ-ДД)")
+        send_message('Когда надо сдать все лабы?(Формат: ГГГГ-ММ-ДД)')
         @bot.listen do |answer|
-          if valid_date?(answer.text) == false then sendMessage(
+          if valid_date?(answer.text) == false then send_message(
             "#{@name}, ты неверно ввел дату!")
           else
             @end_date = Date.parse(answer.text)
 
             if time_left(@start_date, @end_date) == true then
-            sendMessage("В запасе дней:#{@remainder}")
+            send_message("Понял, на все про все у нас:#{@remainder}")
             else
-              sendMessage("Время вышло")
+              send_message('Время вышло')
             end
             break
           end
@@ -89,10 +92,30 @@ class Semester < BaseClass
 end
 
 
+class Subject < BaseClass
+  def run
+    send_message('Какой предмет учим?')
+    bot.listen do |answer|
+      @task = answer.text
+      send_message('Сколько лаб надо сдать?')
+      bot.listen do |answer|
+        if !/\d+/.match(answer.text) == true then send_message("#{@name}, введи число!")
+        else
+          send_message('ОК.')
+          break
+        end
+      end
+      break
+    end
+  end
+end
+
+
+
 
 class Reset < BaseClass
   def run
-    sendMessage("Твои данные удалены")
+    send_message("#{@name}, Твои данные удалены")
   end
 end
 
@@ -104,9 +127,11 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
     when '/start'
       Start.new(bot, message).run
     when '/semester'
-      Semester.new(bot, message).first_step
+      Semester.new(bot, message).run
     when '/reset'
       Reset.new(bot, message).run
+    when '/subject'
+      Subject.new(bot, message).run
 
     when '/stop'
       bot.api.send_message(
